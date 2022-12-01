3b11f70ce5e19fadc79e9055857b801bcf559fef
Justin O'Neill <justinoneill2007@gmail.com>
2022-12-01 16:05:20 -0600

    Initial commit

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+
+	import { setLocale } from '$i18n/i18n-svelte';
+	import type { Locales } from '$i18n/i18n-types';
+	import { loadLocale } from '$i18n/i18n-util.sync';
+
+	import '$scss/main.scss';
+	import 'normalize.css';
+
+	export let data: LayoutData;
+
+	loadLocale(data.language as Locales);
+	setLocale(data.language as Locales);
+</script>
+
+<slot />
